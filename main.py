f076914ece404e64751ab68a00d8b5cc2bcbc2af
Loïc <loic.hernandez@sfr.fr>
2024-11-14 21:47:46 +0100

    add score

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import random
 
 NOMBRE_MIN = 1
 NOMBRE_MAX = 10
-continuer = True
+NB_QUESTIONS = 5
+score = 0
 
 def poser_question():
+    global score
     a = random.randint(NOMBRE_MIN, NOMBRE_MAX)
     b = random.randint(NOMBRE_MIN, NOMBRE_MAX)
     reponse_str = input(f"Calculez {a} + {b} : ")
     if int(reponse_str) == a + b:
         print("Réponse correcte !")
+        score += 1
     else:
         print(f"Réponse incorrecte ! {a} + {b} = {a + b}")
 
-while continuer:
+for i in range(0,NB_QUESTIONS):
+    print(f"Question {i+1}/{NB_QUESTIONS}")
     poser_question()
-    continuer_str = input("Voulez-vous continuer ? (o/n) :")
-    if continuer_str == "n":
-        continuer = False
+    print()
+
+if score == NB_QUESTIONS:
+    print(f"Bravo, vous avez eu {score}/{NB_QUESTIONS} !")
+elif score == 0:
+    print(f"Vous avez eu {score}/{NB_QUESTIONS}... Il va falloir réviser.")
+elif score < NB_QUESTIONS:
+    print(f"Vous avez eu {score}/{NB_QUESTIONS}. Pas mal, mais vous pouvez mieux faire.")
+
