a16e6f8f8321bddec295be11d23d82f1fd62efa1
Loïc <loic.hernandez@sfr.fr>
2024-11-14 22:11:33 +0100

    add differents types of calcul

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
 import random
 
+# Constantes &
 NOMBRE_MIN = 1
 NOMBRE_MAX = 10
 NB_QUESTIONS = 5
 score = 0
+operateur_str = ""
+calcul = 0
+
+# Fonctions
+def addition(a, b):
+    return a + b
+
+def multiplication(a, b):
+    return a * b
+
+def soustraction(a, b):
+    return a - b
+
+def choix_opérateur(a,b):
+    global operateur_str
+    global calcul
+    operation = random.choice([addition, multiplication, soustraction])
+    operateur_str = "+"
+    calcul = addition(a, b)
+
+    if operation == soustraction:
+        operateur_str = "-"
+        calcul = soustraction(a, b)
+    elif operation == multiplication:
+        operateur_str = "x"
+        calcul = multiplication(a, b)
 
 def poser_question():
     global score
+    global calcul
     a = random.randint(NOMBRE_MIN, NOMBRE_MAX)
     b = random.randint(NOMBRE_MIN, NOMBRE_MAX)
-    reponse_str = input(f"Calculez {a} + {b} : ")
-    if int(reponse_str) == a + b:
+    choix_opérateur(a,b)
+
+
+    reponse_str = input(f"Calculez {a} {operateur_str} {b} : ")
+    if int(reponse_str) == calcul:
         print("Réponse correcte !")
         score += 1
     else:
-        print(f"Réponse incorrecte ! {a} + {b} = {a + b}")
+        print(f"Réponse incorrecte ! {a} {operateur_str} {b} = {calcul}")
 
+
+# Programme principal
 for i in range(0,NB_QUESTIONS):
     print(f"Question {i+1}/{NB_QUESTIONS}")
     poser_question()
